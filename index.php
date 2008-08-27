f5dac0ff69c95d4f4ad4df4d068a5ba623c56af5
Lester Caine <lester@lsces.co.uk>
2008-08-27 15:11:11 +0000

    Update base index.php

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * $Header: /cvsroot/bitweaver/_bit_citizen/index.php,v 1.2 2008/08/27 15:11:11 lsces Exp $
+ *
+ * Copyright (c) 2006 bitweaver.org
+ * All Rights Reserved. See copyright.txt for details and a complete list of authors.
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+ *
+ * @package contacts
+ * @subpackage functions
+ */
+
+/**
+ * required setup
+ */
+require_once( '../bit_setup_inc.php' );
+
+include_once( CITIZEN_PKG_PATH.'Citizen.php' );
+
+$gBitSystem->isPackageActive('contacts', TRUE);
+
+$gContent = new Citizen();
+
+if( !empty( $_REQUEST['content_id'] ) ) {
+	$gContent->load($_REQUEST['content_id']);
+}
+
+// Comments engine!
+if( $gBitSystem->isFeatureActive( 'feature_citizen_comments' ) ) {
+	$comments_vars = Array('page');
+	$comments_prefix_var='contact note:';
+	$comments_object_var='page';
+	$commentsParentId = $gContent->mContentId;
+	$comments_return_url = CITIZEN_PKG_URL.'index.php?content_id='.$gContent->mContentId;
+	include_once( LIBERTY_PKG_PATH.'comments_inc.php' );
+}
+
+$gBitSmarty->assign_by_ref( 'citizenInfo', $gContent->mInfo );
+if ( $gContent->isValid() ) {
+	$gBitSystem->setBrowserTitle("Citizen List Item");
+	$gBitSystem->display( 'bitpackage:citizen/show_citizen.tpl', NULL, array( 'display_mode' => 'display' ));
+} else {
+	header ("location: ".CITIZEN_PKG_URL."list.php");
+	die;
+}
+?>
